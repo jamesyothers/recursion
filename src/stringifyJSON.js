d55868ba6becd2650b58c73580b26cf732355187
jamesyothers <james.yothers@gmail.com>
2014-04-30 14:24:43 -0700

    Complete 12/22 tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,35 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(obj);
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  else if (obj === null) {
+    return 'null';
+  }
+  else if (obj === true) {
+    return 'true';
+  }
+  else if (obj === false) {
+    return 'false';
+  }
+  else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  else if (Array.isArray(obj) && obj.length === 0) {
+    return "[]";
+  }
+  else if (Array.isArray(obj)) {
+    var result = "[" + obj.map(function(element,index,obj) {
+      return stringifyJSON(element);    
+    });
+    return result + "]";
+    //[0,1,2,3,4].reduce(function(previousValue, currentValue, index, array){
+    //obj.reduce(function(element,accumulator,index,obj) {
+    //  return accumulator + element;   
+    //});
+    //return "[" + obj + "]";                
+  }
+
 };
