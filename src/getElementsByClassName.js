99957e936460a2b2c1dbbd55d6a4ce2a9a6ea035
jamesyothers <james.yothers@gmail.com>
2014-05-05 09:08:35 -0700

    Section 3 Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,69 +5,24 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  if (!result) {
     var result = [];
-    if (className === document.body.className) {
-      result.push(document.body);
-    }
-    var counter = 0;
-    var elements = document.body.childNodes;
-  }
-  console.log(document.body);
-  console.log(elements);
-
-  for (var i=0; i < elements.length; i++) {
-      console.log(elements[i]);
-    if (elements[i].classList && elements[i].classList.contains(className)) {
-      //console.log(elements[i]);
-      result.push(elements[i]);
-
-      //console.log(elements[i].childNodes);
-      //if (elements[i].childNodes) {
-      //  elements = elements[i].childNodes;
-      //  getElementsByClassName(className);  
-      //} 
-    } 
-
-  }
-/*
-
-    if (elements[i].childNodes.length > 0) {
-      console.log(elements[i].childNodes);
-      elements = elements[i].childNodes;
-      console.log(elements);
-      //getElementsByClassName(className);  
-    }
- /*
-  for (var j=0; j < elements.length; j++) {
-    while (elements[j].childNodes.length > 0) {
-      elements = elements[j].childNodes;
-      //getElementsByClassName(className);
-      if (elements[j].classList && elements[j].classList.contains(className)) {
-        result.push(elements[j]);
-      }
-    }
-
-  }
-*/
-  counter++;
-
-  
-  
-    /*
-    if (elements[i].childNodes.length > 0) {
-      
-      
-      for (var j=0; j < elements[i].childNodes.length; j++) {
-      
-        if (elements[i].childNodes[j] && elements[i].childNodes[j].classList.contains(className)) {
-          //console.log(elements[i].childNodes[j]);
-          result.push(elements[i].childNodes[j]);
+    
+    var checkNode = function(node) {
+        //if the node has the class we're looking for, add it to the result array
+        if (node.classList && node.classList.contains(className)) {
+            result.push(node);
         }
-      }
-    }
-  */
-  
-  return result;
-  
-};
+        
+        //if the node has children, look at each child
+        if (node.childNodes) {
+            for (var i = 0 ; i < node.childNodes.length ; i++ ) {
+                 checkNode(node.childNodes[i]);
+            }
+        }
+        
+    };
+    //start the recursive function on the first page node
+    checkNode(document.body);
+    
+    return result;
+}; 
